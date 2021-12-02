9e45283f7c7b5c813d494cb574fefe1a450cc541
maribelcuales <maribelcuales@gmail.com>
2021-12-02 20:50:15 +0800

    Comments added to smart contract.

--- counter.sol
+++ counter.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity >=0.7.4;
 contract FirstContract {
+  // public variable which maintains the counter  
   int public count = 0;
   function incr() public {
     count++;
@@ -8,6 +9,7 @@ contract FirstContract {
   function decr() public {
     count--;
   }
+  // view function are read only functions 
   function display() public view returns (int) {
     return count; 
   }
