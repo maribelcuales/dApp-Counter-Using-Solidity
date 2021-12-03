5fa722e2eec59243eeca4f9fd17a929d7652f4dc
maribelcuales <maribelcuales@gmail.com>
2021-12-03 22:11:04 +0800

    Created script to deploy local server.

---
+++ Deploy.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const Web3 = require('web3');
+const web3 = new Web3('http://localhost:8545'); 
+
+const bytecode = fs.readFileSync('build/counter_sol_FirstContract.bin'); 
+
+const abi = JSON.parse(fs.readFileSync('build.counter_sol.FirstContract.abi'));
+(async function () {
+  const ganacheAccounts = await web3.eth.getAccounts();
+  const myWalletAddress = ganacheAccounts[0];
+  const myContract = new web3.eth.Contract(abi);
+  myContract.deploy({
+    data: bytecode.toString()
+  }).send({
+    from: myWalletAddress,
+    gas: 5000000
+  }).then((deployment) => {
+    console.log('Voting was successfully deployed!');
+    console.log('Voting can be interfaced with at this address:');
+    console.log(deployment.options.address);
+  }).catch((err) => {
+    console.log(err); 
+  });
+}) ();
