ae5c247a21572b0b8bba85cfce5b3d881a6a6d42
maribelcuales <maribelcuales@gmail.com>
2021-12-04 18:43:20 +0800

    Comments added to code.

--- Deploy.js
+++ Deploy.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545'); 
 
-const bytecode = fs.readFileSync('build/counter_sol_FirstContract.bin'); 
+// bytecode and abi are files built during the compilation of .sol script 
+const bytecode = fs.readFileSync('build/counter_sol_FirstContract.bin');
 
+// abi is a JSON file that describes the deployed contract and its functions 
 const abi = JSON.parse(fs.readFileSync('build.counter_sol.FirstContract.abi'));
+
 (async function () {
   const ganacheAccounts = await web3.eth.getAccounts();
   const myWalletAddress = ganacheAccounts[0];
